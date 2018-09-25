77839d9cf80b6e65924ce7049ae907d9f0ccba5b
Samuel Loury <konubinixweb@gmail.com>
2018-09-25 22:11:45 +0200

    Add the vis.js data inside the package for the time being

--- setup.py
+++ setup.py
@@ -5,13 +5,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='graphwatch',
-    version="0.0.0",
+    version="0.0.2",
     author="Samuel Loury",
     author_email="konubinixweb@gmail.com",
     description="A web app showing a dot graph evolutions.",
     packages=find_packages(),
     zip_safe=True,
     include_package_data=True,
+    package_data={'': [
+        '*.js', '*.css',
+    ]},
     install_requires=[
         "flexx",
         "pydot",
